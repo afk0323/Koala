a2b765b82f03a31323cad547336d9df9fa86e3f8
afk0323 <jwv0323@naver.com>
2022-08-07 11:50:45 +0900

    다이나믹 프로그래밍 2

---
+++ 다이나믹 프로그래밍 2/돌 게임 3/solution.py
@@ -0,0 +1,10 @@
+n = int(input())
+dp = [0] * (n + 1)
+dp[1] = 1; dp[2] = 2; dp[3] = 1; dp[4] = 1
+
+for i in range(5, n+1):
+    if dp[i-1] == 2 or dp[i-3] == 2 or dp[i-4] == 2: dp[i] = 1
+    else: dp[i] = 2
+
+if dp[n] == 1: print('SK')
+else: print('CY')
